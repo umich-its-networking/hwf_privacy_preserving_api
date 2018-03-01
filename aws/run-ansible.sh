1e1f07c7a94920f9c31abe8d2a6c1ed9d5510c39
Nowell <dnowell@m-c02s40rkg8wl.local>
2018-03-01 16:52:44 -0500

    Slight AWS changes for different VPC.  Really should put those in an ignored file someday

--- aws/run-ansible.sh
+++ aws/run-ansible.sh
@@ -2,12 +2,13 @@
 # This assumes you have a profile set in ~/.aws
 # And a EC2 keypair and all that
 # Also make sure to customize the variables in build-ec2.yml
-export AWS_REGION="us-east-1"
+export AWS_REGION="us-east-2"
 export AWS_PROFILE="cloud-ambassadors"
-KEYFILE=~/.ssh/dnowell-ca.pem
+KEYFILE=~/.ssh/dnowell-ca-us-east-2.pem
 ansible-playbook build-ec2.yml
 AWSIP=`cat /tmp/aws_public_ip`
 echo $AWSIP
 cd ../Vagrant
+sleep 45
 ansible-playbook -i "${AWSIP}," playbook.yml --key-file $KEYFILE --user ubuntu -e 'ansible_python_interpreter=/usr/bin/python3'
 
