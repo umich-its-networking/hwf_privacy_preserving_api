36369bacb2fecb235924aad017766709c2027c70
Ostrander <jlost@UMICH.EDU>
2018-03-01 12:04:27 -0500

    Fix app ctx problems

--- api.py
+++ api.py
@@ -3,18 +3,15 @@ from privacy_preserving import PreserveCsv
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/count")
+def count():
+    query = request.args.get('query', '')
+    return g.private_source.count(query)
+
 with app.app_context():
-
-    g["private_source"] = PreserveCsv('/Users/jlost/Projects/privascope/hwf_privacy_preserving_api/public_data/student-por.csv')
-
-    @app.route("/")
-    def hello():
-        return "Hello World!"
-
-    @app.route("/count")
-    def count():
-        query = request.args.get('query', '')
-        return g.private_source.count(query)
-
-if __name__ == "__main__":
+    g.private_source = PreserveCsv('./public_data/student-por.csv')
     app.run()
