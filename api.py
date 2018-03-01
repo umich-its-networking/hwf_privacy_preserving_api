5648a0931157ed92045b4c1a6ae1dd2a9e176995
Ostrander <jlost@UMICH.EDU>
2018-03-01 13:44:50 -0500

    flask_restful + flask_restful_swagger

--- api.py
+++ api.py
@@ -1,21 +1,29 @@
 from flask import Flask, request, g
 from flask_restful import Resource, Api
+from flask_restful_swagger import swagger
 
 from privacy_preserving import PreserveCsv
 
 app = Flask(__name__)
-api = Api(app)
+api = swagger.docs(Api(app), apiVersion='0.1')
 
 class Count(Resource):
+    @swagger.operation(
+        dataType="number",
+        parameters=[
+            {
+              "name": "query",
+              "description": "DataFrame query by which to return a count.",
+              "required": True,
+              "allowMultiple": False,
+              "dataType": "string",
+              "paramType": "path",
+            }
+          ],
+        )
     def get(self, query):
         count = g.private_source.count(query)
         return count
-
-# @app.route("/count")
-# def count():
-#     query = request.args['query']
-#     count = g.private_source.count(query)
-#     return str(count)
 
 api.add_resource(Count, '/count/<string:query>')
 
