15926805055b320a0378e93614a9b29b3d7125cb
Kris Steinhoff <steinhof@umich.edu>
2018-03-01 14:53:28 -0500

    Abstract Count class

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, g, redirect
+from flask import Flask, redirect
 from flask_restful import Resource, Api
 from flask_restful_swagger import swagger
 
@@ -8,11 +8,16 @@ from privacy_preserving import Preserve
 app = Flask(__name__)
 api = swagger.docs(Api(app), apiVersion='0.1', api_spec_url='/spec')
 
+
 @app.route('/')
 def home():
     return redirect("/spec.html")
 
+
 class Count(Resource):
+    def __init__(self):
+        pass
+
     @swagger.operation(
         dataType="number",
         parameters=[
@@ -27,11 +32,16 @@ class Count(Resource):
           ],
         )
     def get(self, query):
-        source = Preserve(pandas.read_csv('./public_data/student-por.csv'))
-        count = source.count(query)
+        count = self._data.count(query)
         return count
 
-api.add_resource(Count, '/count/<string:query>')
+
+class CountStudent(Count):
+    def __init__(self):
+        self._data = Preserve(pandas.read_csv('./public_data/student-por.csv'))
+
+
+api.add_resource(CountStudent, '/student/count/<string:query>')
 
 
 if __name__ == "__main__":
