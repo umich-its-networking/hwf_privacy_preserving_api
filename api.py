56fb808f5b4c43b15965c4e72c02e22faa431f01
Ostrander <jlost@UMICH.EDU>
2018-03-01 13:29:31 -0500

    Basics of flask-restful

--- api.py
+++ api.py
@@ -1,17 +1,23 @@
 from flask import Flask, request, g
+from flask_restful import Resource, Api
+
 from privacy_preserving import PreserveCsv
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+class Count(Resource):
+    def get(self, query):
+        count = g.private_source.count(query)
+        return count
 
-@app.route("/count")
-def count():
-    query = request.args['query']
-    count = g.private_source.count(query)
-    return str(count)
+# @app.route("/count")
+# def count():
+#     query = request.args['query']
+#     count = g.private_source.count(query)
+#     return str(count)
+
+api.add_resource(Count, '/count/<string:query>')
 
 with app.app_context():
     g.private_source = PreserveCsv('./public_data/student-por.csv')
