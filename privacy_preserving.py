7ce2a7db573d43d13cadfd91c41a6e73abee9635
Kris Steinhoff <steinhof@umich.edu>
2018-03-01 13:28:56 -0500

    Add random jitter

--- privacy_preserving.py
+++ privacy_preserving.py
@@ -1,18 +1,39 @@
 import pandas as pd
+import numpy as np
+from zlib import crc32
 
 
 class Preserve(object):
+    def __init__(self, dataframe):
+        self._data = dataframe
 
-    _df = None
+    def _protect(self, val, query):
+        jitter = 2
+        np.random.seed(crc32(query.encode()))
 
-    def __init__(self):
-        pass
+        if val <= jitter:
+            jitter_range = (0, (jitter * 2) + 1)
+        else:
+            jitter_range = (val - jitter, val + jitter + 1)
+
+        protected_val = np.random.randint(*jitter_range)
+
+        return protected_val
+
+    def attributes(self):
+        return dict(zip(
+            self._data.columns,
+            list(map(str, list(self._data.dtypes)))
+        ))
+
+    def count(self, query):
+        return self._protect(
+            len(self._data.query(query)),
+            query
+        )
 
 
 class PreserveCsv(Preserve):
 
-    def __init__(self, csv_file_path, **kw_args):
-        self._df = pd.read_csv(csv_file_path, **kw_args)
-
-    def count(self, query):
-        return len(self._df.query(query))
+    def __init__(self, *args, **kw_args):
+        return pd.read_csv(*args, **kw_args)
