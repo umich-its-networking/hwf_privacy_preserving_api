b0ab15d09870c7bf91314ca4f07e1098d0b98c90
Ostrander <jlost@UMICH.EDU>
2018-03-01 11:41:15 -0500

    Nonworking API code

--- api.py
+++ api.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, request, g
+from privacy_preserving import PreserveCsv
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+with app.app_context():
+
+    g["private_source"] = PreserveCsv('/Users/jlost/Projects/privascope/hwf_privacy_preserving_api/public_data/student-por.csv')
+
+    @app.route("/")
+    def hello():
+        return "Hello World!"
+
+    @app.route("/count")
+    def count():
+        query = request.args.get('query', '')
+        return g.private_source.count(query)
 
 if __name__ == "__main__":
     app.run()
